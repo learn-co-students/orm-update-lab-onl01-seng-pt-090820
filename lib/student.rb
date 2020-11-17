0a8ed3b029a01c77fcadf2dcc6f6c8857d524f83
musiccity365 <m_sackstein@yahoo.com>
2020-11-17 16:56:06 -0500

    commit tree 0856a7dabc7252558fea6cb8de8fa015dc8ebbef

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,11 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name, grade, id=nil)
+    @name, grade, id = name, grade, id
+  end
 
 end
